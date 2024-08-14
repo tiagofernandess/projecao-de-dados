bfef21b115eaf6553c7f82874179d11aca033428
tiagofernandess <tiagofernandes0095@gmail.com>
2024-08-13 22:40:41 -0300

    adicionado ideal meses

--- script.js
+++ script.js
@@ -10,12 +10,31 @@ const inputCommission = document.querySelector("#royal-commission-three-months")
 const inputExpendes = document.querySelector("#real-expenses-three-months");
 const total = document.querySelector("#actual-total-three-months");
 
+const determinedIdealThreeMonths = document.querySelector("#determined-ideal-three-months")
+const luckyThreeMonths = document.querySelector("#luck-three-months-ideal")
+const idealCommissionthreeMonths = document.querySelector("#ideal-commission-three-months")
+const idealExpensesThreeMonths = document.querySelector("#ideal-expenses-three-months")
+const totalThree = document.querySelector("#total-value-three-months")
+
+
+
 function process() {
     const prohibitedValue = parseFloat(prohibited.value) || 0;
     const luckValue = parseFloat(luck.value) || 0;
     const commissionValue = parseFloat(commission.value) || 0;
     const expensesValue = parseFloat(expenses.value) || 0;
-    
+
+    const averageLuck = 0.72
+    const averageExpenses = 0.9
+    const averageCommission = 0.8
+    const monthlyProjection = 3
+   
+
+    const luckConvertedThreeMonths = prohibitedValue * averageLuck
+    const comissionConvertedThreeMonths = commissionValue * averageCommission
+    const expensesConvertedThreeMonths = expensesValue * averageExpenses
+
+
     inputProhibited.textContent = `Apurado (R$): ${prohibitedValue.toFixed(2)}`;
     inputLuck.textContent = `Sorte (R$): ${luckValue.toFixed(2)}`;
     inputCommission.textContent = `Comissão (R$): ${commissionValue.toFixed(2)}`;
@@ -24,11 +43,21 @@ function process() {
     const totalValue = prohibitedValue - expensesValue - commissionValue - luckValue;
     total.textContent = `Total (R$): ${totalValue.toFixed(2)}`;
 
-    if (totalValue < 0) {
-        totalValue.style.color = red;
-    }
+
+    luckyThreeMonths.textContent = `Sorte (R$): ${luckConvertedThreeMonths.toFixed(2)}`;
+    idealCommissionthreeMonths.textContent = `Comissão (R$): ${comissionConvertedThreeMonths.toFixed(2)}`;
+    idealExpensesThreeMonths.textContent = `Despesas (R$): ${expensesConvertedThreeMonths.toFixed(2)}`;
+    determinedIdealThreeMonths.textContent = `Apurado (R$): ${prohibitedValue.toFixed(2)}`;
+
+
+    const determinedIdealValue = prohibitedValue;
+    const totalValueTree = determinedIdealValue - comissionConvertedThreeMonths - expensesConvertedThreeMonths - luckConvertedThreeMonths;
+
+    totalThree.textContent = `Total (R$): ${totalValueTree.toFixed(2)}`;
+
 }
 
-button.addEventListener("click", process);
+
+button.addEventListener("click", process);
 
 
